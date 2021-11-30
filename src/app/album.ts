985a2c0952fd0afccbd7382bd7a77da6dd4a2e95
Chinwendu <enyinnapc16127@gmail.com>
2021-11-29 21:21:40 -0800

    Completed module

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,7 +1,8 @@
-import { Track } from './track'
+import { Track } from './track';
+
 export interface Album {
-    name: string,
-    releaseDate: string,
-    coverImage: string,
-    tracks: Track[]
+    name: string;
+    releaseDate: string;
+    coverImage: string;
+    tracks: Track[];
 }
